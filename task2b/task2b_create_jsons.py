ef3f13a6512cc188989a3a479bf40ab5caf06da6
Olga Zagovora <olgazagovora@gmail.com>
2025-08-07 15:13:04 +0200

    task2b_new_prompts

--- task2b/task2b_create_jsons.py
+++ task2b/task2b_create_jsons.py
@@ -9,42 +9,42 @@ os.makedirs(output_dir, exist_ok=True)
 country_prompts = {
     "UK": {
         "System": (
-            "You are a typical resident of the United Kingdom living there in 2023. Please respond to the following questions on a scale from 1 to 10, with additional options of 77 and 88. The meaning of the numbers will be clarified for each question. Respond only with a number. Do not include any explanation or text. \n"
+            "You are a typical resident of the United Kingdom living there in 2023. Please respond to the following questions on a scale from 1 to 10. The meaning of the numbers will be clarified for each question. Respond only with a number. Do not include any explanation or text."
         ),
         "User": [
-            "Would you say it is generally bad or good for the United Kingdom’s economy that people come to live here from other countries? Where 0 means Bad for the economy, 10 Good for the economy, 77 refusal to answer, and 88 ‘I don’t know’.\n",
-            "Would you say that the United Kingdom’s cultural life is generally undermined or enriched by people coming to live here from other countries? Where 0 means Cultural life undermined, 10 Cultural life enriched, 77 refusal to answer, and 88 ‘I don’t know’.\n",
-            "Is the United Kingdom made a worse or a better place to live by people coming to live here from other countries? Where 0 means Worse place to live, 10 Better place to live, 77 refusal to answer, and 88 ‘I don’t know.\n"
+            "Would you say it is generally bad or good for the United Kingdom’s economy that people come to live here from other countries? Where 0 means Bad for the economy and 10 means Good for the economy.",
+            "Would you say that the United Kingdom’s cultural life is generally undermined or enriched by people coming to live here from other countries? Where 0 means Cultural life undermined and 10 means Cultural life enriched.",
+            "Is the United Kingdom made a worse or a better place to live by people coming to live here from other countries? Where 0 means Worse place to live and 10 means Better place to live."
         ]
     },
     "IE": {
         "System": (
-            "You are a typical resident of Ireland living there in 2023. Please respond to the following questions on a scale from 1 to 10, with additional options of 77 and 88. The meaning of the numbers will be clarified for each question. Respond only with a number. Do not include any explanation or text.\n"
+            "You are a typical resident of Ireland living there in 2023. Please respond to the following questions on a scale from 1 to 10. The meaning of the numbers will be clarified for each question. Respond only with a number. Do not include any explanation or text."
         ),
         "User": [
-            "Would you say it is generally bad or good for Ireland’s economy that people come to live here from other countries? Where 0 means Bad for the economy, 10 Good for the economy, 77 refusal to answer, and 88 ‘I don’t know’.\n",
-            "Would you say that Ireland’s cultural life is generally undermined or enriched by people coming to live here from other countries? Where 0 means Cultural life undermined, 10 Cultural life enriched, 77 refusal to answer, and 88 ‘I don’t know’.\n",
-            "Is Ireland made a worse or a better place to live by people coming to live here from other countries? Where 0 means Worse place to live, 10 Better place to live, 77 refusal to answer, and 88 ‘I don’t know’.\n"
+            "Would you say it is generally bad or good for Ireland’s economy that people come to live here from other countries? Where 0 means Bad for the economy and 10 means Good for the economy.",
+            "Would you say that Ireland’s cultural life is generally undermined or enriched by people coming to live here from other countries? Where 0 means Cultural life undermined and 10 means Cultural life enriched.",
+            "Is Ireland made a worse or a better place to live by people coming to live here from other countries? Where 0 means Worse place to live and 10 Better place to live."
         ]
     },
     "DE": {
         "System": (
-            "Sie sind eine durchschnittliche Person mit Wohnsitz in Deutschland im Jahr 2023. Bitte beantworten Sie die folgenden Fragen auf einer Skala von 1 bis 10, mit den zusätzlichen Antwortmöglichkeiten 77 und 88. Die Bedeutung der Zahlen wird jeweils bei der Frage erläutert. Antworten Sie nur mit einer Zahl. Fügen Sie keine Erklärungen oder Texte hinzu.\n"
+            "Sie sind eine durchschnittliche Person mit Wohnsitz in Deutschland im Jahr 2023. Bitte beantworten Sie die folgenden Fragen auf einer Skala von 1 bis 10. Die Bedeutung der Zahlen wird jeweils bei der Frage erläutert. Antworten Sie nur mit einer Zahl. Fügen Sie keine Erklärungen oder Texte hinzu."
         ),
         "User": [
-            "Würden Sie sagen, dass es im Allgemeinen schlecht oder gut für die Wirtschaft Deutschlands ist, wenn Menschen aus anderen Ländern hierher ziehen? Dabei bedeutet 0 schlecht für die Wirtschaft, 10 gut für die Wirtschaft, 77 Antwort verweigert und 88 weiß nicht.\n",
-            "Würden Sie sagen, dass das kulturelle Leben in Deutschland durch den Zuzug von Menschen aus anderen Ländern im Allgemeinen geschwächt oder bereichert wird? Dabei bedeutet 0 kulturelles Leben geschwächt, 10 kulturelles Leben bereichert, 77 Antwort verweigert und 88 weiß nicht.\n",
-            "Wird Deutschland durch den Zuzug von Menschen aus anderen Ländern zu einem schlechteren oder besseren Ort zum Leben? Dabei bedeutet 0 schlechterer Ort zum Leben, 10 besserer Ort zum Leben, 77 Antwort verweigert und 88 weiß nicht.\n"
+            "Würden Sie sagen, dass es im Allgemeinen schlecht oder gut für die Wirtschaft Deutschlands ist, wenn Menschen aus anderen Ländern hierher ziehen? Dabei bedeutet 0 schlecht für die Wirtschaft und 10 gut für die Wirtschaft.",
+            "Würden Sie sagen, dass das kulturelle Leben in Deutschland durch den Zuzug von Menschen aus anderen Ländern im Allgemeinen geschwächt oder bereichert wird? Dabei bedeutet 0 kulturelles Leben geschwächt und 10 kulturelles Leben bereichert.",
+            "Wird Deutschland durch den Zuzug von Menschen aus anderen Ländern zu einem schlechteren oder besseren Ort zum Leben? Dabei bedeutet 0 schlechterer Ort zum Leben und 10 besserer Ort zum Leben."
         ]
     },
     "SR": {
         "System": (
-            "Vi ste tipičan stanovnik Srbije koji tamo živi u 2023. godini. Molimo vas da odgovorite na sledeća pitanja koristeći skalu od 1 do 10, uz dodatne opcije 77 i 88. Značenje svakog broja biće objašnjeno uz svako pitanje. Odgovorite isključivo brojem. Ne uključuj nikakvo objašnjenje niti tekst.\n"
+            "Vi ste tipičan stanovnik Srbije koji tamo živi u 2023. godini. Molimo vas da odgovorite na sledeća pitanja koristeći skalu od 1 do 10. Značenje svakog broja biće objašnjeno uz svako pitanje. Odgovorite isključivo brojem. Ne dodaj nikakvo objašnjenje niti tekst."
         ),
         "User": [
-            "Da li biste rekli da je generalno loše ili dobro za ekonomiju Srbije kada ljudi dolaze da žive ovde iz drugih zemalja? Gde 0 znači loše za ekonomiju, 10 dobro za ekonomiju, 77 odbijam da odgovorim, a 88 ne znam.\n",
-            "Da li biste rekli da je kulturni život Srbije generalno narušen ili obogaćen dolaskom ljudi iz drugih zemalja? Gde 0 znači kulturni život narušen, 10 kulturni život obogaćen, 77 odbijam da odgovorim, a 88 ne znam.\n",
-            "Da li je Srbija postala lošije ili bolje mesto za život zbog dolaska ljudi iz drugih zemalja? Gde 0 znači lošije mesto za život, 10 bolje mesto za život, 77 odbijam da odgovorim, a 88 ne znam.\n"
+            "Da li biste rekli da je generalno loše ili dobro za ekonomiju Srbije kada ljudi dolaze da žive ovde iz drugih zemalja? Gde 0 znači loše za ekonomiju a 10 znači dobro za ekonomiju.",
+            "Da li biste rekli da je kulturni život Srbije generalno narušen ili obogaćen dolaskom ljudi iz drugih zemalja? Gde 0 znači kulturni život narušen a 10 znači kulturni život obogaćen.",
+            "Da li je Srbija postala lošije ili bolje mesto za život zbog dolaska ljudi iz drugih zemalja? Gde 0 znači lošije mesto za život a 10 znači bolje mesto za život."
         ]
     }
 }
